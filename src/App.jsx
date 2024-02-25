989fe41099fe4c12fab69bdb0dcc819aed811004
rahul488 <myself.rahul78@gmail.com>
2024-02-25 19:48:22 +0530

    change meta name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { Box } from '@mui/material';
+import { useEffect } from 'react';
 import './App.css';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useLocation } from 'react-router-dom';
 import { router } from './routes';
-import { AppDrawer } from './Components';
 import { AppThemeProvider } from './Context';
 
+
 function App() {
+  useEffect(() => {
+    document.title = `Rahul | Portfolio`;
+  }, []);
   return (
     <AppThemeProvider>
       <RouterProvider router={router} />
